edad8b5f3fc426eb0e98683df9586eba3ba4ce9d
hiralkukadia <hiral.kukadia@strathmore.edu>
2025-05-28 16:48:04 +0300

    added a background animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import { BackgroundGradientAnimationDemo } from "@/components/BackgroundGradientAnimation";
 import { NavbarDemo } from "@/components/Navbar";
 import React from "react";
 
 export default function Home() {
   return (
+    <>
     <NavbarDemo />
+    <BackgroundGradientAnimationDemo>
+    </BackgroundGradientAnimationDemo>
+    </>
   );
 }
